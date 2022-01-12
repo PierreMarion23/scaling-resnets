31486c6f0b4a2c81ac39309c5f42d2e00e3bdfbe
PierreMarion23 <pierre23.marion@free.fr>
2022-01-12 11:10:54 +0100

    Clean implementation (2).
    Simplify over-complicated functions in model.py.

--- model.py
+++ model.py
@@ -1,17 +1,9 @@
+import copy
+
 import numpy as np
 import pytorch_lightning as pl
 import torch
 from torch import nn
-
-
-# This is useful to fix the random seed for linear layer initialization.
-# TODO: clean this up.
-def create_linear_layer(in_features, out_features):
-    layer = nn.Linear(in_features, out_features)
-    k = 1 / torch.sqrt(torch.Tensor([in_features]))
-    layer.weight = nn.Parameter(2 * k * torch.rand((out_features, in_features)) - k)
-    layer.bias = nn.Parameter(2 * k * torch.rand((out_features,)) - k)
-    return layer
 
 
 class ResNet(pl.LightningModule):
@@ -27,13 +19,13 @@ class ResNet(pl.LightningModule):
         self.train_init = model_config['train_init']
         self.train_final = model_config['train_final']
 
-        self.init = create_linear_layer(self.initial_width, self.width)
+        self.init = nn.Linear(self.initial_width, self.width)
         if not self.train_init:
             self.init.weight.requires_grad = False
             self.init.bias.requires_grad = False
         self.layers = nn.Sequential(
             *[nn.Linear(self.width, self.width) for _ in range(self.depth)])
-        self.final = create_linear_layer(self.width, self.final_width)
+        self.final = nn.Linear(self.width, self.final_width)
         if not self.train_final:
             self.final.weight.requires_grad = False
             self.final.bias.requires_grad = False
@@ -57,16 +49,9 @@ class ResNet(pl.LightningModule):
     def configure_optimizers(self):
         return torch.optim.RMSprop(filter(lambda p: p.requires_grad, self.parameters()), lr=0.005)
 
-    # TODO: replace this function with a standard one (using state_dict).
     def copy(self):
         result = ResNet(self.initial_width, self.final_width, **self._model_config)
-        result.init.weight = nn.Parameter(self.init.weight.detach().clone())
-        result.init.bias = nn.Parameter(self.init.bias.detach().clone())
-        for k in range(self.depth):
-            result.layers[k].weight = nn.Parameter(self.layers[k].weight.detach().clone())
-            result.layers[k].bias = nn.Parameter(self.layers[k].bias.detach().clone())
-        result.final.weight = nn.Parameter(self.final.weight.detach().clone())
-        result.final.bias = nn.Parameter(self.final.bias.detach().clone())
+        result.load_state_dict(copy.deepcopy(self.state_dict()))
         return result
 
 
@@ -78,6 +63,7 @@ def polyfit(series, degree):
     return poly_approx, error
 
 
+# TODO: vectorize the for loops.
 def denoise_weights_bias(layers, degree):
     depth = len(layers)
     width = layers[0].weight.shape[0]
