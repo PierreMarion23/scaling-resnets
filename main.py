0ac8e6c3dffae4381220b44a319aeb712150f15a
PierreMarion23 <pierre23.marion@free.fr>
2022-01-13 09:10:52 +0100

    Refactor main.py for scalable experiment launches.

--- main.py
+++ main.py
@@ -19,26 +19,38 @@ def fit(config_name, offline):
     if offline:
         os.environ['WANDB_MODE'] = 'offline'
     wandb.login()
-    wandb.init(project='scaling-resnets', entity='lpsm-deep')
 
     config_dict = getattr(config, config_name)
+    name_template = config_dict['name']
 
-    train_dl, test_dl, data_size, nb_classes = data.load_dataset(config_dict['dataset'])
+    for dataset in config_dict['dataset']:
+        name = name_template.replace('dataset', dataset)
+        with wandb.init(project='scaling-resnets', entity='lpsm-deep', name=name) as run:
+            wandb.config = config_dict
 
-    resnet = model.ResNet(initial_width=data_size, final_width=nb_classes, **config_dict['model'])
+            train_dl, test_dl, data_size, nb_classes = data.load_dataset(dataset)
 
-    gpu = 1 if torch.cuda.is_available() else 0
+            resnet = model.ResNet(initial_width=data_size, final_width=nb_classes, **config_dict['model'])
 
-    wandb_logger = pl.loggers.WandbLogger()
-    trainer = pl.Trainer(
-        gpus=gpu,
-        max_epochs=config_dict['epochs'],
-        progress_bar_refresh_rate=20,
-        logger=wandb_logger,
-        callbacks=[logs.PrintingCallback(test_dl)],
-        checkpoint_callback=False
-    )
-    trainer.fit(resnet, train_dl)
+            gpu = 1 if torch.cuda.is_available() else 0
+            device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+            wandb_logger = pl.loggers.WandbLogger()
+            trainer = pl.Trainer(
+                gpus=gpu,
+                max_epochs=config_dict['epochs'],
+                progress_bar_refresh_rate=20,
+                logger=wandb_logger,
+                callbacks=[logs.PrintingCallback(test_dl, device)],
+                checkpoint_callback=False
+            )
+            trainer.fit(resnet, train_dl)
+
+            model_artifact = wandb.Artifact('resnet', type='model', metadata=config_dict)
+            model_artifact.aliases.append(name)
+            with model_artifact.new_file("trained-model.ckpt", mode="wb") as file:
+                torch.save(resnet, file)
+            run.log_artifact(model_artifact)
 
 
 if __name__ == '__main__':
