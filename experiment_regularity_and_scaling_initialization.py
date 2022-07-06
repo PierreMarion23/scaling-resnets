6bdde51f2f6811c16dca0670a33a53da51a15a91
PierreMarion23 <pierre23.marion@free.fr>
2022-07-06 15:38:53 -0700

    clean exp 2

--- experiment_regularity_and_scaling_initialization.py
+++ experiment_regularity_and_scaling_initialization.py
@@ -1,4 +1,7 @@
 import distutils.spawn
+import os
+from typing import Optional
+
 from matplotlib import rc
 import matplotlib.pyplot as plt
 import numpy as np
@@ -72,8 +75,10 @@ def run_experiment(config: dict, grid_beta: list, grid_reg: list) -> list:
     return results
 
 
-def plot_results(results: list):
-    """Reproduces Figure 7 of the paper.
+def plot_results(results: list, filepath: Optional[str] = 'figures'):
+    """Plot heatmaps which describes the hidden state and gradient norms,
+    as a function of scaling and initialization regularity.
+    See Figure 7 of the paper.
 
     :param results: list of results
     :return:
@@ -88,43 +93,49 @@ def plot_results(results: list):
                          values='log_hidden_state_difference', dropna=False)
     df1.index = np.round(df1.index.astype(float), 2)
     df1.columns = np.round(df1.columns.astype(float), 2)
-    df1 = df1.fillna(16)
+    df1.replace(np.inf, np.nan, inplace=True)
+    df1.fillna(16, inplace=True)
     sns.heatmap(df1[::-1], vmin=-1, vmax=1, center=0, xticklabels=5,
                 yticklabels=6, square=True)
     plt.savefig(
-        'figures/heatmap-scaling-regularity-output.pdf', bbox_inches='tight')
+        os.path.join(filepath, 'heatmap-scaling-regularity-output.pdf'),
+        bbox_inches='tight')
     plt.show()
 
     df2 = df.pivot_table(index='scaling', columns='regularity',
                          values='log_gradients_difference', dropna=False)
     df2.index = np.round(df2.index.astype(float), 2)
     df2.columns = np.round(df2.columns.astype(float), 2)
-    df2 = df2.fillna(16)
+    df2.replace(np.inf, np.nan, inplace=True)
+    df2.fillna(16, inplace=True)
     sns.heatmap(df2[::-1], vmin=-1, vmax=1, center=0, xticklabels=5,
                 yticklabels=6, square=True)
     plt.savefig(
-        'figures/heatmap-scaling-regularity-gradient.pdf', bbox_inches='tight')
+        os.path.join(filepath, 'heatmap-scaling-regularity-gradient.pdf'),
+        bbox_inches='tight')
     plt.show()
 
 
 if __name__ == '__main__':
 
-    # Plot examples of fractional Brownian Motion
+    # Plot examples of fractional Brownian Motion - Figure 6 of the paper
     grid_H = [0.2, 0.5, 0.8]
+    filepath = 'figures/regularity_and_scaling_initialization'
+    os.makedirs(filepath, exist_ok=True)
     for hurst in grid_H:
         path = utils.generate_fbm(1000, hurst)[0]
         plt.plot(path)
+        plt.savefig(
+            os.path.join(filepath, 'fbm_examples-{:.1f}.pdf'.format(hurst)),
+            bbox_inches='tight')
         plt.show()
 
     # Run the experiments to loop over various initializations and scalings
+    # Figure 7 of the paper
     config_heatmap = config.scaling_regularity_initialization_exp
 
     grid_beta = list(np.linspace(0, 1.3, 70))
     grid_reg = list(np.linspace(0.05, 0.97, 51))
 
     results = run_experiment(config_heatmap, grid_beta, grid_reg)
-    plot_results(results)
-
-
-
-
+    plot_results(results, filepath)
